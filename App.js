97f2beb4e00bf8412325ac5db04f522ebdf6b9aa
Walid Wazini <wazini25@gmail.com>
2021-09-10 22:06:13 +0800

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet, Text, View,
+  TextInput, Button
+} from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([])
+  
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+  const addGoalHanlder = () => {
+    console.log(enteredGoal)
+    setCourseGoals( currentGoals => [...courseGoals, enteredGoal])
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer} >
+        <TextInput
+          placeholder='Course Goal' style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+          />
+        <Button title='ADD' onPress={addGoalHanlder} />
+        ...
+      </View>
+      <View>
+        {courseGoals.map((goal) => {
+          return <Text key={goal} >{goal}</Text>
+        })} 
+      </View>
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -18,4 +46,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'red',
+    borderWidth: 1,
+    padding: 10
+  }
+
 });
