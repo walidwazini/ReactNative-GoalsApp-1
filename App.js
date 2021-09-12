8f872d5bca261be5145e9b4cb51850dae9b84b4f
Walid Wazini <wazini25@gmail.com>
2021-09-12 22:47:38 +0800

    setup delete func & assigned id to item

--- App.js
+++ App.js
@@ -10,12 +10,21 @@ import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([])
-
   const addGoalHanlder = (goalTitle) => {
     setCourseGoals(currentGoals => [...courseGoals, {
       id: Math.random().toString(), val: goalTitle
     }])
   }
+  const removeGOalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      // goal in arg is object in currentGoals
+      // id that match the goalId going to be remove
+      // id that didn't match going to be filtered (not deleted)
+      return currentGoals.filter((goal) => goal.id !== goalId)
+      // id need to fwd to the GoalItem
+    })
+  }
+
 
   return (
     <View style={styles.screen}>
@@ -24,7 +33,7 @@ export default function App() {
       keyExtractor={(item, index) => item.id}
         // FlatList automatically add keys to our item
         data={courseGoals} renderItem={itemData => (
-         <GoalItem onDelete={() => console.log('Test Delete')} itemTitle={itemData.item.val} />
+         <GoalItem id={itemData.item.id} onDelete={() => console.log('Test Delete')} itemTitle={itemData.item.val} />
         )} />
 
       {/* <Text>Open up App.js to start working on your app!</Text>
